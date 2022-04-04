f6d04bc0ba659706b0e20380590133f3245086a4
Natanonkhuanata <101494144+Natanonkhuanata@users.noreply.github.com>
2022-04-04 15:25:24 +0700

    Create EP2.py

---
+++ EP2.py
@@ -0,0 +1,12 @@
+"""
+การออกเบบการใช้ for ร่วมกับ if
+"""
+
+
+sum = 0
+for i in range(1,5):
+    num = int(input(f"จำนวนเงินที่ {i} >>>")) # การใช้คำสร้าง print ใน input
+    sum += num
+
+print(sum)
+print(sum/4)
