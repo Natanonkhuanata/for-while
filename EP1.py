68b5aad135a3aa8fd6a3cfe1097bd11553746ecb
Natanonkhuanata <101494144+Natanonkhuanata@users.noreply.github.com>
2022-04-04 14:48:52 +0700

    Create EP1.py

---
+++ EP1.py
@@ -0,0 +1,17 @@
+"""
+เรื่อง for-while การวนลูปหรือทำซ้ำ
+"""
+# เข้าสู้การเขียนโค้ด (ไม่ใช่เเบบบฝึกหัด)
+
+from re import I
+
+
+x = range(10) #0-9
+Y = range(1,10) #1-9
+E = range(100,200) #100-199
+
+for i in E:
+    print(i)
+
+for i in Y:
+    print(i)
