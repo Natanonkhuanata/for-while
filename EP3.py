e5c54025d22196dc4a26cf1647967729a5885834
Natanonkhuanata <101494144+Natanonkhuanata@users.noreply.github.com>
2022-04-07 13:12:21 +0700

    Update EP3.py

--- EP3.py
+++ EP3.py
@@ -1,14 +1,14 @@
 """
 การใช้ for ที่น่าสนใจ
 """
-base = float(input("เลขฐาน>>>"))
-power = int(input("เลขยกกำลัง (เป็นจำนวนเต็ม)>>>"))
-
-if power >= 0:
-    result = 1
-    for i in range(0,power):
-        print(i)
-        result *= base
 
 
-print(result)
+
+x = int(input("เลขมา 1 จำนวน >>>"))
+
+if x <10:
+    for i in range(1,5):
+        x += 2
+        if x % 2 ==  0:
+            print(x)
+
